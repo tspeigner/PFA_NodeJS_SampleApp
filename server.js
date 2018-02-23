fe3b88a8e6e0339db6458279ba218c30609ef39f
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-02-23 14:37:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World from Distelli. Your Node.js application is working!\n');
-}).listen(8004);
+}).listen(80);
 
 console.log('Server started');
