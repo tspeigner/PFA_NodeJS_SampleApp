b1717e589781ba86f51766f22ab5cf4c18b2c60a
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-04-27 11:36:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Tieto Breakfast @ Stockholm from Puppet Pipelines. Your Node.js application is working!\n');
+    response.end('Hello VMware Breakfast from Puppet Pipelines. Your Node.js application is working!\n');
 }).listen(80);
 
 console.log('Server started');
