44f930fd7e47bc6cc05fd3ec36305d110d2dd2a4
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-04-30 10:26:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(function (request, response) {
+httphghg.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello VMware from Puppet Pipelines. Your Node.js application is working!\n');
 }).listen(80);
