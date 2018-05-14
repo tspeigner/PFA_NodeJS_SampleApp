f347adf9b0ae706028deac297b549f2459edea09
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-14 10:08:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-httpbngvngn.createServer(function (request, response) {
+http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello Puppet Forecast Call from Puppet Pipelines. Your Node.js application is working!\n');
 }).listen(80);
