dd6cbe431845b0791af8e8ab60269541f8228b80
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-17 13:36:23 +0300

    Updated my code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Puppet Forecast Call from Puppet Pipelines. Your Node.js application is working!\n');
+    response.end('Hello Helsinki from Puppet Pipelines. Your Node.js application is working!\n');
 }).listen(80);
 
 console.log('Server started');
