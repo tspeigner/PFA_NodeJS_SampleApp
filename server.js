6e5e3059d1560aefcee839c58800ffb59885e2de
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2018-05-17 17:54:39 +0300

    Updated to port 80

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello Prove from Puppet Pipelines. Your Node.js application is working!\n');
-}).listen(8080);
+}).listen(80);
 
 console.log('Server started');
