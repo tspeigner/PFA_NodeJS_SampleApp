646f33ae8b5816448f453f269da3d50a346fca97
Tommy Speigner <tspeigner@gmail.com>
2018-05-29 12:16:10 -0700

    interpolation

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     var ip = request.headers["X-Forwarded-For"] || request.connection.remoteAddress;
-    response.end('Hello Portland from Puppet Pipelines. Your Node.js application is working!\n' + "\n Your client ip is");
+    response.end('Hello Portland from Puppet Pipelines. Your Node.js application is working!\n' + "\n Your client ${ip} is");
 }).listen(80);
 
 console.log('Server started');
