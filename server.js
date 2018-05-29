2dda238088b50e7eb030e429e33c59423607856c
Tommy Speigner <tspeigner@gmail.com>
2018-05-29 12:06:04 -0700

    moved ip variable into body.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
-var ip = req.headers["X-Forwarded-For"] || req.connection.remoteAddress;
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
+    var ip = request.headers["X-Forwarded-For"] || request.connection.remoteAddress;
     response.end('Hello Portland from Puppet Pipelines. Your Node.js application is working!\n' + ip);
 }).listen(80);
 
