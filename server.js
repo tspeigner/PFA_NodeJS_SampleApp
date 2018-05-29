1bab3209ef6bb1cedfa0992464b7c739a9b6e68d
Tommy Speigner <tspeigner@gmail.com>
2018-05-29 14:56:33 -0700

    adding hostname into app

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
 if (ip.substr(0, 7) == "::ffff:") {ip = ip.substr(7)};
-    response.end('Hello Portland from Puppet Pipelines. Your Node.js application is working!\n' + `\n Your client IP ${ip} is.`);
+    response.end('Hello Portland from Puppet Pipelines. Your Node.js application is working!\n' + `\n Your client IP ${ip} is.` + `\n And your hostname is ${h}.`);
     dns.lookup(h, { hints: dns.ADDRCONFIG }, function(err, ip) {
     console.log('IP: ' + ip);
     dns.lookupService(ip, 0, function (err, hostname, service) {
