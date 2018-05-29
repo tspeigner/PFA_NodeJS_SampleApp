28262fa3e3edd49cd3f44a40a63e01ee7f4dcd3a
Tommy Speigner <tspeigner@gmail.com>
2018-05-29 12:27:14 -0700

    adding new ip logic
    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
    if (ip.substr(0, 7) == "::ffff:") {
      ip = ip.substr(7)
    }

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    var ip = request.headers["X-Forwarded-For"] || request.connection.remoteAddress;
+    var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+if (ip.substr(0, 7) == "::ffff:") {ip = ip.substr(7)};
     response.end('Hello Portland from Puppet Pipelines. Your Node.js application is working!\n' + `\n Your client IP ${ip} is.`);
 }).listen(80);
 
