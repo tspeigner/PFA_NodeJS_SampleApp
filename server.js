71a6a0ac2a4b11939bbfe7fdaa81bdcf1295bf75
Tommy Speigner <tspeigner@gmail.com>
2018-05-29 12:03:28 -0700

    adding ip to output

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
+var ip = req.headers["X-Forwarded-For"] || req.connection.remoteAddress;
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Portland from Puppet Pipelines. Your Node.js application is working!\n');
+    response.end('Hello Portland from Puppet Pipelines. Your Node.js application is working!\n' + ip);
 }).listen(80);
 
 console.log('Server started');
