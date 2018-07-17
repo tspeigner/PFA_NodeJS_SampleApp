bfd536cfb247006363d7261f8995047e82988c54
Tommy Speigner <tspeigner@gmail.com>
2018-07-17 11:34:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     var ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
 if (ip.substr(0, 7) == "::ffff:") {ip = ip.substr(7)};
-    response.end('Hello Pechanga from Puppet Pipelines. Your Node.js application is working!\n' + `\n Your client IP ${ip} is.` + `\n You are viewing content on this server: ${h}.`);
+    response.end('Hello Puppet from Puppet Pipelines. Your Node.js application is working!\n' + `\n Your client IP ${ip} is.` + `\n You are viewing content on this server: ${h}.`);
 }).listen(80);
 
 console.log('Server started');
